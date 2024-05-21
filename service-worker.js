3b1a9a475d7c7b107ca9cce6c4ad206d231c3741
Raymond <leow.ray@gmail.com>
2024-05-21 19:52:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6ba49c4d71abeb1fb95d95e3f096d00.js"
+  "/precache-manifest.cf8bd2fc07ade45f3aba2ac761099a7c.js"
 );
 
 self.addEventListener('message', (event) => {
