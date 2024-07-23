34fd8e65b13cdacff970934a112924864b86673d
Raymond <leow.ray@gmail.com>
2024-07-24 00:30:33 +0200

    fix: temp replace 'about' section text

--- src/components/innerpages/About.js
+++ src/components/innerpages/About.js
@@ -93,9 +93,11 @@ class About extends React.Component {
   render() {
     const heading = "About Raymond";
     const name = "Raymond Leow";
-    const description =
+    /* const description =
       "On this page, I'll share about myself and what I love." +
-      " There's a handful of things I quite enjoy: drawing, cooking, working out and karaoke.";
+      " There's a handful of things I quite enjoy: drawing, cooking, working out and karaoke."; */
+    const description =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     const drawing = "I love drawing, it goes down in the family.";
     const cooking = "I love cooking, based on my dad.";
     const fitness =
@@ -123,9 +125,9 @@ class About extends React.Component {
                   <Heading>{heading}</Heading>
                   <VerticalSpacer />
                   <Description>{description}</Description>
-                  <Description>{drawing}</Description>
+                  {/* <Description>{drawing}</Description>
                   <Description>{cooking}</Description>
-                  <Description>{fitness}</Description>
+                  <Description>{fitness}</Description> */}
                 </TextColumn>
               </Row>
             </AboutContainer>
